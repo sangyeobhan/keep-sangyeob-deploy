2286edd26d2b52924abfb42c7cd44745f66434eb
sangyeobhan <sangyeobhandev@gmail.com>
2023-11-09 18:10:01 -0500

    changed file structure based on the requirements

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,39 @@
-import React from "react";
-
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Body from "./Body";
+import InputForm from "./InputForm";
+import Note from "./Note";
+import { notes } from "../notes";
 
 export default function App() {
+    const [noteArray, setNotes] = useState(notes);
+
+    const deleteNote = (key) => {
+        const updatedNoteArray = noteArray.filter((note) => note.key !== key);
+        setNotes(updatedNoteArray);
+    };
+
+    const addNote = (note) => {
+        const updatedNoteArray = [...noteArray, { key: Date.now(), ...note }];
+        setNotes(updatedNoteArray);
+    };
     return (
         <div className="app">
             <Header />
-            <Body />
+            <div className="body">
+                <InputForm addNote={addNote} />
+                <div className="notes-container">
+                    {noteArray.map((note) => {
+                        return (
+                            <Note
+                                key={note.key}
+                                note={note}
+                                hanleDeleteButton={() => deleteNote(note.key)}
+                            />
+                        );
+                    })}
+                </div>
+            </div>
             <Footer />
         </div>
     );
